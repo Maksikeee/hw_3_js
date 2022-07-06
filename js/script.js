1954342abe938de005c710f750c0ebb2a9c7b06c
Maksikeee <teakobeats@gmail.com>
2022-07-06 21:37:42 +0300

    hw_1_js ready

---
+++ js/script.js
@@ -0,0 +1,84 @@
+//Задание 2
+console.log('Задание 2');
+let a1 = 5 % 3,
+	 a2 = 3 % 5,
+	 a3 = 5 + '3',
+	 a4 = '5' - 3,
+	 a5 = 75 + 'кг',
+	 a6 = 785 * 653,
+	 a7 = 100 / 25,
+	 a8 = 0 * 0,
+	 a9 = 0 / 2,
+	 a10 = 89 / 0,
+	 a11 = 98 + 2,
+	 a12 = 5 - 98,
+	 a13 = (8 + 56 * 4) / 5,
+	 a14 = (9 - 12) * 7 / (5 + 2),
+	 a15 = +"123",
+	 a16 = 1 || 0,
+	 a17 = false || true,
+	 a18 = true > 0;
+
+console.log(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18);
+console.log('------------------------------');
+
+//Задание 3
+console.log('Задание 3');
+let width = 10,
+	 heigth = 23,
+	 SPryam = width * heigth;
+console.log(SPryam);
+console.log('------------------------------');
+
+//Задание 4
+console.log('Задание 4');
+let VCilindra = Math.PI * Math.pow(a7, 2);
+console.log(VCilindra);
+console.log('------------------------------');
+
+//Задание 5
+console.log('Задание 5');
+let r = 5,
+	 VKruga = Math.PI * Math.pow(r, 2);
+console.log(VKruga);
+console.log('------------------------------');
+
+//Задание 6
+console.log('Задание 6');
+let a = 5,
+	 b = 7,
+	 h = 10,
+	 STrap = (a + b) / 2 * h;
+console.log(STrap);
+console.log('------------------------------');
+
+//Задание 7
+console.log('Задание 7');
+let years = 5,
+	 p = 10,
+	 S = 2000000,
+	 ezhMesStavka = p / 12 / 100;
+	 obshStavka = Math.pow((1 + ezhMesStavka), (years * 12));
+	 ezhMesPlatezh = (S * ezhMesStavka * obshStavka) / (obshStavka - 1);
+	 Pereplata = ezhMesPlatezh * years * 12 - S;
+console.log(Pereplata);
+console.log('------------------------------');
+
+//Задание 8
+console.log('Задание 8');
+let aa = 8,
+	 bb = 3;
+	 x = (16 - aa) / 2 + bb;
+console.log(x);
+
+let z = (15 * bb - a) / (6 - bb);
+console.log(z);
+
+let y = 23780 / (3 + aa + bb);
+console.log(y);
+console.log('------------------------------');
+
+
+
+
+
