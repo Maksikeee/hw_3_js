a31228084545fb77e6007ab3032597ebbf762db4
Maksikeee <teakobeats@gmail.com>
2022-07-14 18:42:58 +0300

    hw3 js ready

--- js/script.js
+++ js/script.js
@@ -1,163 +1,141 @@
-// Задание 1
-let name = prompt('ваше имя');
-	 age = prompt('возраст');
-	 city = prompt('город проживания');
-	 phone = prompt('телефон');
-	 email = prompt('почта');
-	 company = prompt('место работы');
-console.log(`Меня зовут ${name}. Мне ${age} лет. Я проживаю в городе ${city} и работаю в компании ${company}. Мои контактные данные: ${phone}, ${email}.`);
+//задание 1
+console.log(`\n --------------Задание 1--------------`);
+let z;
+for (z=1; z<=50; z++) {
+	console.log(z);
+}
 
 //Задание 2
-let bday = 2022 - age;
-console.log(`${name} родился в ${bday} году.`);
+let i = 89;
+while(i>=11){
+	
+	document.write(`${i} </br>`);
+	i--;
+}
 
 //Задание 3
-let str = prompt('Введите строку из 6-ти цифр');
-	 str1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
-	 str2 = Number(str[3]) + Number(str[4]) + Number(str[5]);
-str1 == str2 ? alert('Да') : alert('Нет');
+console.log(`\n --------------Задание 3--------------`);
+let sum = 0;
+for(i=0; i<=100; i++){
+	sum += i;
+	
+}
+console.log(sum);
 
 //Задание 4
-let a = prompt('Введите число');
-a > 0 ? alert('Верно') : alert('Неверно');
+console.log(`\n --------------Задание 4--------------`);
+sum = 0;
+for(i=1; i<=5; i++){
+	for(let j=0; j<=i; j++){
+		sum += j;
+	}
+	console.log(sum);
+	sum = 0;
+}
+
 
 //Задание 5
-a = 10;
-let b = 2;
+console.log(`\n --------------Задание 5--------------`);
 
-if(a + b > 1) {
-	alert(`Сумма: ${a + b}, разность: ${a - b}, произведение: ${a * b}, частное: ${a / b}, квадрат: ${Math.pow(a + b, 2)}.`);
-} else {
-	alert(`Сумма: ${a + b}, разность: ${a - b}, произведение: ${a * b}, частное: ${a / b}.`);
+for(i=8; i<=56; i++){
+	if(i%2===0){
+		console.log(i)
+	}
+}
+
+i = 8;
+while(i<=56){
+	if(i%2===0){
+		console.log(i);
+		i++;
+	} else {
+		i++;
+	}
 }
 
 //Задание 6
-if(a > 2 && a < 11 || b >= 6 && b < 14) {
-	alert('Верно');
-} else {
-	alert('Неверно');
+console.log(`\n --------------Задание 6--------------`);
+for(i=2; i<=10; i++){
+	for(j=1; j<=10; j++){
+		console.log(`${i} * ${j} = ${i*j}`);
+	}
+	console.log('\n');
 }
 
 //Задание 7
-let n = prompt('Введите число от 0 до 59');
-
-switch(true) {
-	case n >= 0 && n <=14 :
-		alert('Первая четверть часа');
-		break;
-
-	case n >= 15 && n <=29 :
-		alert('Вторая четверть часа');
-		break;
-
-	case n >= 30 && n <=44 :
-		alert('Третья четверть часа');
-		break;
-
-	case n >= 45 && n <=59 :
-		alert('Четвертая четверть часа');
-		break;
-
-	default:
-		alert('Неверное значение.');
+console.log(`\n --------------Задание 7--------------`);
+let result;
+for(let n=1000; n>=50;){
+	n = n/2;
+	if(n<50){
+		console.log(n);
+	}else{
+		continue;
+	}
 }
 
 //Задание 8
-let day = prompt('Введите число от 1 до 31');
-
-switch(true) {
-
-	case day >=1 && day <= 10 :
-		alert('Первая декада месяца');
+let sum8 = 0;
+let numbers8 = 0;
+while(true){
+	let number8 = +prompt('Введите число');
+	if(isNaN(number8) && number8 !==''){
+		alert('Введено не число');
+	}else if(number8===0 || number8===''){
 		break;
-
-	case day >=11 && day <= 20 :
-		alert('Вторая декада месяца');
-		break;
-
-	case day >=21 && day <= 31 :
-		alert('Третья декада месяца');
-		break;
-
-	default :
-		alert('Неверное число');
+	}else{
+		sum8+=number8;
+		numbers8+=1;
+	}
 }
+let mean = sum8/numbers8;
+console.log(mean);
+console.log(sum8);
 
-//Задание 9
-let days = prompt('Введите число');
-	 year = days / 365;
-	 month = days % 365 / 31;
-	 week = days % 365 % 31 / 7;
-	 day = days % 365 % 31 % 7;
-	 hour = days % 1 *24;
-	 minutes = hour % 1 * 60;
-	 seconds = minutes % 1 * 60;
+// //Задание 9
+console.log(`\n --------------Задание 9--------------`);
+let str = '4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57';
+let arr = str.split(' ').map(Number);
+let max = arr[0];
+let min = arr[0];
 
-
-
-
-switch(true) {
-
-	case days < 0 :
-		console.log('Неверное значение.')
-		break;
-
-	case true :
-		switch(true){
-			case year >= 0 && year < 1 :
-				console.log('Меньше года');
-				break;
-			case year >= 1 :
-				console.log(`${Math.trunc(year)} year`);
-				break;
-		}
-
-	case true :
-		switch(true) {
-			case month >= 0 && month < 1 :
-				console.log('Меньше месяца');
-				break;
-			case month >= 1 :
-				console.log(`${Math.trunc(month)} month`);
-				break;
-		}
-
-	case true :
-		switch(true) {
-			case week >= 0 && week < 1 :
-				console.log('Меньше недели');
-				break;
-			case week >= 1 :
-				console.log(`${Math.trunc(week)} week`);
-				break;
-		}
-
-	case day >= 1 :
-		console.log(`${Math.trunc(day)} day, ${Math.trunc(minutes)} minutes, ${Math.trunc(seconds)} seconds.`);
-		break;
-
+for(i=0; i<arr.length-1; i++){
+	if(arr[i]>max){
+		max = arr[i];
+	}else if(arr[i]<min){
+		min = arr[i];
+	}
 }
+console.log(max);
+console.log(min);
 
 //Задание 10
+console.log(`\n --------------Задание 10--------------`);
 
-let month1 = Math.ceil(days % 365 / 31);
+let str10 = prompt('Введите число');
+let arr10 = str10.split('').map(Number);
+let numbers = 0;
+let sum10 = 0;
 
-switch(true) {
-	case month1 >= 3 && month1 <= 5 :
-		console.log(`${month1} месяц года Весна`);
-		break;
-
-	case month1 >= 6 && month1 <= 8 :
-		console.log(`${month1} месяц года Лето`);
-		break;
-
-	case month1 >= 9 && month1 <= 11 :
-		console.log(`${month1} месяц года Осень`);
-		break;
-
-	default :
-		console.log(`${month1} месяц года Зима`);
+console.log(`\n Разбиваем число на цифры`);
+for(i=0; i<str10.length; i++){
+	console.log(arr10[i]);
+	numbers += 1;
 }
+console.log(`\n Кол-во цифр в числе`);
+console.log(numbers);
+
+console.log(`\n Сумма цифр в числе`);
+for(i=0; i<str10.length; i++){
+	sum10 += arr10[i];
+}
+console.log(sum10);
+
+console.log(`\n Меняет порядок числа на обратный`);
+for(i=str10.length-1; i>=0; i--){
+	console.log(arr10[i]);
+}
+
 
 
 
